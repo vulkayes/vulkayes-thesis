b65bc3ef7a93516a4b8befe611980da51cd4eded
Eduard Lavuš <lavusedu@fel.cvut.cz>
2020-03-04 21:00:10 +0100

    add pdfs

--- misc/gen.sh
+++ misc/gen.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-pandoc \
-	--highlight-style "misc/code.theme" --css "misc/style.css" \
-	--from markdown --to pdf --standalone --pdf-engine=xelatex \
-	"documents/$1.md" --output "pdfs/$1.pdf" \
-&& open "pdfs/$1.pdf"
+function render() {
+	local document=$1
+
+	pandoc \
+		--highlight-style "misc/code.theme" --css "misc/style.css" \
+		--from markdown --to pdf --standalone --pdf-engine=xelatex \
+		"documents/$document.md" --output "pdfs/$document.pdf"
+}
+
+and_open=${2:-1}
+if [ "$and_open" -ne 0 ]; then 
+	render "$1" && open "pdfs/$1.pdf"
+else
+	render "$1"
+fi
---
+++ misc/gen_all.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+misc/gen.sh drop_order 0
+misc/gen.sh implementation_plan 0
+misc/gen.sh swapchain_recreate 0
