b14e48c219ff847575fc1cb2be58293db6589e83
TheEdward162 <theedward162@gmail.com>
2020-04-28 18:24:49 +0200

    teapot - add run benches

---
+++ codes/teapot-benches/run_benches.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+PLATFORM=$1
+DEVICE_INDEX=${2:-0}
+
+rm Cargo.toml
+ln -s ST-Cargo.toml Cargo.toml
+VY_TEAPOT_DEVICE_INDEX=$DEVICE_INDEX cargo run --bin ash --release > "benches/${PLATFORM}/raw_ash.txt"
+VY_TEAPOT_DEVICE_INDEX=$DEVICE_INDEX cargo run --bin vulkayes --release > "benches/${PLATFORM}/raw_vulkayes_ST.txt"
+
+rm Cargo.toml
+ln -s MT-Cargo.toml Cargo.toml
+VY_TEAPOT_DEVICE_INDEX=$DEVICE_INDEX cargo run --bin vulkayes --release > "benches/${PLATFORM}/raw_vulkayes_MT.txt"
